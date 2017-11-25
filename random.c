f15d28fc2707810d171a9fed55596b4c9bcb0293
Benjamin Garrett <bgarrett@temple.edu>
2017-11-25 16:08:03 -0500

    Reformatted source.

--- random.c
+++ random.c
@@ -1,9 +1,14 @@
 #include "random.h"
 
-int64_t rand_in_range(int64_t min, int64_t max){
-    
-    int64_t g;
-    
-    g = min + (double)(max - min) * (double)rand() / (double)RAND_MAX;
-    return g;
+int64_t rand_in_range(int64_t min, int64_t max) {
+
+  int64_t g;
+
+  g = min + (double) (max - min) * (double) rand() / (double) RAND_MAX;
+  return g;
+}
+
+double random_double() {
+
+  return (double) rand() / (double) RAND_MAX;
 }
--- random.h
+++ random.h
@@ -6,19 +6,20 @@
  */
 
 #ifndef RANDOM_H
-#define	RANDOM_H
+#define RANDOM_H
 
-#ifdef	__cplusplus
+#ifdef __cplusplus
 extern "C" {
 #endif
 
-    #include<stdint.h>
-    #include<stdlib.h>
-    int64_t rand_in_range(int64_t min, int64_t max);
+#include<stdint.h>
+#include<stdlib.h>
+  int64_t rand_in_range(int64_t min, int64_t max);
+  double random_double();
 
-#ifdef	__cplusplus
+#ifdef __cplusplus
 }
 #endif
 
-#endif	/* RANDOM_H */
+#endif /* RANDOM_H */
 
