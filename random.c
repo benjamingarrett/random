02311898773a0caa9086d6e3214aca298bebb034
Benjamin Garrett <bgarrett@temple.edu>
2019-09-16 15:12:17 -0400

    Commented out minor bug about log from math.h NOTE: must fix random exponential

--- random.c
+++ random.c
@@ -14,7 +14,9 @@ double random_double(){
 
 int64_t rand_in_range_exponential(int64_t min, int64_t max, double lambda){
   double d = random_double();
-  double x = (1.0) / (-1.0) * log(d) / lambda;
+  //double x = (1.0) / (-1.0) * log(d) / lambda;
+  double x=0.0;
+  fprintf(stderr,"Careful! rand_in_range_exponential is down for repairs\n");
   //printf("%f\n", x);
   //double z = (double)min + (double)(max - min) * x;
   //printf("%f %ld\n", z, (int64_t)z);
