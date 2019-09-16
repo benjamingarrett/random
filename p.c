a74f8361853f80a9d74c71b4f32ccb90ff85498b
Benjamin Garrett <bgarrett@temple.edu>
2019-09-15 21:05:38 -0400

    HPC prep (loops,printf)

--- p.c
+++ p.c
@@ -6,18 +6,22 @@
 int main(){
   int capacity = 40;
   int c[capacity];
-  for(int64_t g = 0; g < capacity; g++){
+  int64_t g;
+  FILE * fp;
+  fp=fopen("random_execution_trace.log","a");
+  for(g = 0; g < capacity; g++){
     c[g] = 0;
   }
   int64_t max_x = 0;
-  for(int64_t g = 0; g < 100000; g++){
+  for(g = 0; g < 100000; g++){
     int64_t x = rand_in_range_exponential(1, 5, 0.9);
     max_x = (max_x < x) ? x : max_x;
-    printf("%ld\n", x);
+    fprintf(fp,"%ld\n", x);
     c[x-1]++;
   }
-  printf("%ld\n", max_x);
-  for(int64_t g = 0; g < capacity; g++){
-    printf("%ld\n", c[g]);
+  fprintf(fp,"%ld\n", max_x);
+  for(g = 0; g < capacity; g++){
+    fprintf(fp,"%ld\n", c[g]);
   }
+  fclose(fp);
 }
